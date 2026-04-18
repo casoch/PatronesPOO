a6a06a8046dcdb15c652e0b46ca911e53a5f02b4
casoch <casoch@gmail.com>
2026-04-18 17:01:45 +0200

    Patrón observer sólo con Cliente

---
+++ src/Observer/Cliente.java
@@ -0,0 +1,30 @@
+package Observer;
+
+import java.util.Objects;
+
+public class Cliente implements IObservadorStock {
+    private String nombre;
+    private String email;
+
+    public Cliente(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    @Override
+    public void actualizar(String nombreProducto) {
+        System.out.println("Aviso para " + nombre + " (" + email + "): "
+                + "el producto \"" + nombreProducto + "\" ya vuelve a estar en stock.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Cliente cliente)) return false;
+        return Objects.equals(email, cliente.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+}
---
+++ src/Observer/IObservadorStock.java
@@ -0,0 +1,8 @@
+package Observer;
+
+/**
+ * Esta es la interfaz observer
+ */
+public interface IObservadorStock {
+    public abstract void actualizar(String nombreProducto);
+}
